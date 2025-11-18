50ad9d97be3468f2cf6eeabbbb5598550653ac51
edublackf <edublackf@gmail.com>
2025-11-17 19:23:52 -0500

    Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind"; // <-- 1. LA IMPORTACIÓN
 
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()] // <-- 2. LA INTEGRACIÓN

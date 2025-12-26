10415b9b3db3e98dfee8bb84403c6c594e0b26e5
edublackf <edublackf@gmail.com>
2025-12-26 02:45:26 -0500

    actulizacion capacidades

---
+++ src/data/teamData.js
@@ -0,0 +1,91 @@
+export const teamMembers = {
+    // 1. Directores
+    "rudy-laguna": {
+        name: "Rudy Laguna",
+        role: "Director",
+        image: "/rudy-laguna.png",
+        slug: "rudy-laguna"
+    },
+    "viviana-sanchez": {
+        name: "Viviana Sánchez",
+        role: "Directora",
+        image: "/viviana-sanchez.png",
+        slug: "viviana-sanchez"
+    },
+
+    // 2. Especialistas (Fila 1)
+    "alvaro-gordillo": {
+        name: "Alvaro Gordillo",
+        role: "Especialista en Inversión y Desarrollo",
+        image: "/alvaro-gordillo.png",
+        slug: "alvaro-gordillo"
+    },
+    "fernando-jauregui": {
+        name: "Fernando Jáuregui",
+        role: "Especialista en Inversiones y Modelación Econométrica",
+        image: "/fernando-jauregui.png",
+        slug: "fernando-jauregui"
+    },
+
+    // 3. Especialistas (Fila 2)
+    "alex-carbajal": {
+        name: "Alex Carbajal",
+        role: "Especialista en Evaluación de Proyectos",
+        image: "/alex-carbajal.png",
+        slug: "alex-carbajal"
+    },
+    "sergio-villegas": {
+        name: "Sergio Villegas",
+        role: "Especialista en Inversiones",
+        image: "/sergio-villegas.png",
+        slug: "sergio-villegas"
+    },
+    "elmer-huaman": {
+        name: "Elmer Huamán",
+        role: "Especialista en Inversión para la Competitividad y el Desarrollo",
+        image: "/elmer-huaman.png",
+        slug: "elmer-huaman"
+    },
+
+    // 4. Especialistas (Fila 3)
+    "karla-casas": {
+        name: "Karla Casas",
+        role: "Jefe de Proyectos en Planeamientos, Competitividad y Desarrollo",
+        image: "/karla-casas.png",
+        slug: "karla-casas"
+    },
+    "evelyn-sanchez": {
+        name: "Evelyn Sánchez",
+        role: "Especialista en Economía Aplicada y Evaluación de Proyectos",
+        image: "/evelyn-sanchez.png",
+        slug: "evelyn-sanchez"
+    },
+    "melissa-sanchez": {
+        name: "Melissa Sánchez",
+        role: "Especialista en Administración",
+        image: "/melissa-sanchez.png",
+        slug: "melissa-sanchez"
+    },
+
+    // 5. Especialistas (Fila 4)
+    "jorge-ruiz": {
+        name: "Jorge Ruiz",
+        role: "Especialista en Planes de Desarrollo e Investigación",
+        image: "/jorge-ruiz.png",
+        slug: "jorge-ruiz"
+    },
+    "mario-huaman": {
+        name: "Mario Huamán",
+        role: "Especialista en Estudios Económicos",
+        image: "/mario-huaman.png",
+        slug: "mario-huaman"
+    }
+};
+
+// Datos de las industrias para reutilizar en el sidebar
+export const industriesData = {
+    "mineria": { title: "Minería", image: "/industrias/mineria.jpg", link: "/industrias/mineria" },
+    "agricultura": { title: "Agricultura", image: "/industrias/agricultura.jpg", link: "/industrias/agricultura" },
+    "educacion": { title: "Educación", image: "/industrias/educacion.jpg", link: "/industrias/educacion" },
+    "infraestructura": { title: "Infraestructura", image: "/industrias/infraestructura.jpg", link: "/industrias/infraestructura" },
+};

8ffbe4dcc85bed1e10f79f4764ef5321c1c9922c
edublackf <edublackf@gmail.com>
2026-05-22 18:22:10 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // En Astro 5, el comportamiento 'hybrid' ya es el predeterminado
-  output: 'server', // o 'hybrid'
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind()]
 });
